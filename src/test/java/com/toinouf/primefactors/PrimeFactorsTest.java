f364d3948d4f91da156c080d8d05d698fa47a29d
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-04-02 21:34:53 +0200

    Init PrimeFactors kata

---
+++ src/test/java/com/toinouf/primefactors/PrimeFactorsTest.java
@@ -0,0 +1,13 @@
+package com.toinouf.primefactors;
+
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class PrimeFactorsTest {
+
+    @Test
+    public void testApp() {
+        assertThat(true).isTrue();
+    }
+}
